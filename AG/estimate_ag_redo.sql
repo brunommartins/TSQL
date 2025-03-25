003230ad56a7e0575be153ba4099b5b5e783ac0b
brunommartins <brunommartins@live.com>
2025-03-25 09:21:12 -0300

    Update estimate_ag_redo.sql

--- AG/estimate_ag_redo.sql
+++ AG/estimate_ag_redo.sql
@@ -71,3 +71,38 @@ JOIN sys.availability_groups ag
     ON ar.group_id = ag.group_id
 WHERE drs.is_local = 0  -- Foca nos dados da réplica secundária
 ORDER BY AG_Name, Database_Name;
+
+
+
+
+
+--- query simples
+
+
+SELECT 
+    ag.name AS AG_Name,
+    ar.replica_server_name AS Replica_Name,
+    drs.database_id,
+    db_name(drs.database_id) AS Database_Name,
+    drs.log_send_queue_size AS Log_Send_Queue_Size_KB,  -- Tamanho da fila de envio de logs (KB)
+    drs.log_send_rate AS Log_Send_Rate_KBps,           -- Taxa de envio de logs (KB/s)
+    drs.redo_queue_size AS Redo_Queue_Size_KB,         -- Tamanho da fila de redo (KB)
+    drs.redo_rate AS Redo_Rate_KBps,                   -- Taxa de processamento do redo (KB/s)
+    CASE 
+        WHEN drs.log_send_rate > 0 
+        THEN CAST(drs.log_send_queue_size / drs.log_send_rate AS DECIMAL(10,2))
+        ELSE NULL 
+    END AS Estimated_Log_Send_Time_Seconds,            -- Tempo estimado para envio (s)
+    CASE 
+        WHEN drs.redo_rate > 0 
+        THEN CAST(drs.redo_queue_size / drs.redo_rate AS DECIMAL(10,2))
+        ELSE NULL 
+    END AS Estimated_Redo_Time_Seconds                 -- Tempo estimado para aplicação do redo (s)
+FROM sys.dm_hadr_database_replica_states drs
+JOIN sys.availability_replicas ar 
+    ON drs.replica_id = ar.replica_id
+JOIN sys.availability_groups ag 
+    ON ar.group_id = ag.group_id
+WHERE drs.is_local = 0  -- Foca nos dados da réplica secundária
+ORDER BY AG_Name, Database_Name;
+
