fa2a8a295d68090f31b72e41dbb7bb0a6f7ee299
brunommartins <brunommartins@live.com>
2025-04-18 09:42:28 -0300

    Update script_conf_maxdop.sql

--- MAXDOP/script_conf_maxdop.sql
+++ MAXDOP/script_conf_maxdop.sql
@@ -18,14 +18,40 @@ EXEC sp_configure 'show advanced options', 0; RECONFIGURE;
 
 */
 
-    
+/*
+-------------------------------------------------------------------------------------
+  Script: Diagnóstico e Sugestão de MAXDOP e Cost Threshold
+  Autor: Bruno de Melo Martins
+  Versão: 1.0
+  Data: 2025-04-18
 
+  Descrição:
+  Este script coleta informações de CPU e NUMA do ambiente SQL Server, recomenda 
+  valores para as configurações 'max degree of parallelism' e 
+  'cost threshold for parallelism' com base em boas práticas da Microsoft.
 
--- Habilita opções avançadas temporariamente
-EXEC sp_configure 'show advanced options', 1;
-RECONFIGURE;
+  Observação:
+  O script preserva o valor original da opção 'show advanced options', restaurando
+  ao final o estado inicial.
 
--- Variáveis
+-------------------------------------------------------------------------------------
+*/
+
+-- Armazena valor atual de 'show advanced options'
+DECLARE @showAdvancedOriginal INT;
+
+SELECT @showAdvancedOriginal = CAST(value_in_use AS INT)
+FROM sys.configurations
+WHERE name = 'show advanced options';
+
+-- Ativa temporariamente se necessário
+IF @showAdvancedOriginal = 0
+BEGIN
+    EXEC sp_configure 'show advanced options', 1;
+    RECONFIGURE;
+END;
+
+-- Variáveis principais
 DECLARE 
     @cpu_count INT,
     @numa_nodes INT,
@@ -35,16 +61,16 @@ DECLARE
     @atual_cost_threshold INT,
     @sugestao_cost_threshold INT = 25;
 
--- Coleta informações de hardware
+-- Coleta informações do sistema
 SELECT 
     @cpu_count = cpu_count,
     @numa_nodes = numa_node_count
 FROM sys.dm_os_sys_info;
 
--- Calcula CPUs por NUMA
+-- Cálculo CPUs por NUMA
 SET @cpus_por_numa = @cpu_count / @numa_nodes;
 
--- Sugestão para MAXDOP com base em boas práticas
+-- Lógica de sugestão de MAXDOP
 IF @cpu_count <= 8
     SET @sugestao_maxdop = @cpu_count;
 ELSE
@@ -54,18 +80,16 @@ ELSE
             ELSE 8 
         END;
 
--- Valores atuais
-SELECT 
-    @atual_maxdop = CAST(value_in_use AS INT)
+-- Coleta valores atuais
+SELECT @atual_maxdop = CAST(value_in_use AS INT)
 FROM sys.configurations
 WHERE name = 'max degree of parallelism';
 
-SELECT 
-    @atual_cost_threshold = CAST(value_in_use AS INT)
+SELECT @atual_cost_threshold = CAST(value_in_use AS INT)
 FROM sys.configurations
 WHERE name = 'cost threshold for parallelism';
 
--- Diagnóstico
+-- Exibição dos resultados
 PRINT '--- Diagnóstico do ambiente ---';
 PRINT 'CPUs totais: ' + CAST(@cpu_count AS VARCHAR);
 PRINT 'NUMA nodes: ' + CAST(@numa_nodes AS VARCHAR);
@@ -73,14 +97,27 @@ PRINT 'CPUs por NUMA: ' + CAST(@cpus_por_numa AS VARCHAR);
 PRINT 'MaxDOP atual: ' + CAST(@atual_maxdop AS VARCHAR);
 PRINT 'Cost Threshold atual: ' + CAST(@atual_cost_threshold AS VARCHAR);
 
--- Sugestões
 PRINT '--- Sugestões com base em boas práticas ---';
 PRINT 'MaxDOP sugerido: ' + CAST(@sugestao_maxdop AS VARCHAR);
 PRINT 'Cost Threshold sugerido: ' + CAST(@sugestao_cost_threshold AS VARCHAR);
 
--- Gera comandos
 PRINT '--- Comandos sugeridos para aplicar as configurações ---';
 PRINT 'EXEC sp_configure ''show advanced options'', 1; RECONFIGURE;';
 PRINT 'EXEC sp_configure ''max degree of parallelism'', ' + CAST(@sugestao_maxdop AS VARCHAR) + '; RECONFIGURE;';
 PRINT 'EXEC sp_configure ''cost threshold for parallelism'', ' + CAST(@sugestao_cost_threshold AS VARCHAR) + '; RECONFIGURE;';
-PRINT 'EXEC sp_configure ''show advanced options'', 0; RECONFIGURE;';
+-- Restaura valor original de 'show advanced options'
+IF @showAdvancedOriginal = 0
+BEGIN
+    PRINT 'EXEC sp_configure ''show advanced options'', 0; RECONFIGURE;';
+END
+
+-- Volta a configuração original
+IF @showAdvancedOriginal = 0
+BEGIN
+    EXEC sp_configure 'show advanced options', 0;
+    RECONFIGURE;
+END
+ELSE
+BEGIN
+    PRINT '-- show advanced options estava ativado; nenhuma ação necessária.';
+END
